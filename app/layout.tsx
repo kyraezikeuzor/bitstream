ed3b9da53982c924af1bd64fd51d332062df40d1
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2025-07-06 22:55:27 -0500

    Removed unused link var

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 import { Toaster } from "@/components/ui/sonner";
 import Footer from "@/components/ui/footer";
 
