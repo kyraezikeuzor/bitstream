5a56bc541477d2cd018bcdf5e4114b24ee903961
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2025-07-06 22:50:32 -0500

    Added site title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Hack Club CDN",
+  title: "Bitstream | CDN",
   description:
     "Upload any file URL to get a permanent link that will last for thousands of years.",
 };
